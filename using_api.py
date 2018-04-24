45b04d67b61c61f313569d5c72ede9e35f05c4ea
Vinicius Zanchini <vinicius978@gmail.com>
2018-04-23 22:57:38 -0300

    insercao de classe que conecta com rest API do twitter

--- using_api.py
+++ using_api.py
@@ -1,33 +1,44 @@
+import tweepy
 from tweepy import Stream
 from tweepy import OAuthHandler
-from tweepy.streaming import StreamListener
 import time
-
+import json
 
 consumer_key = 'PmwzArtWhHWGEMWlmC6eOoaGV'
 consumer_secret = '4o3cLDGGXYaxOhhFv8uKKd1roAr6UatmWOYfzjLqmW02g6hWCM'
-access_token = 	'986059156036706306-VnRTpYkm0qvgbdUD1urxleZjzUYZtur'
-access_token_secret = '	kWyh6GBfGzOBBW2tRDNtnK7M4ussrsVYtoBpmpSNcC92A'
+access_token = '986059156036706306-VnRTpYkm0qvgbdUD1urxleZjzUYZtur'
+access_token_secret = 'kWyh6GBfGzOBBW2tRDNtnK7M4ussrsVYtoBpmpSNcC92A'
 
-class listener(StreamListener):
+
+class Listener(tweepy.StreamListener):
 
     def on_data(self, data):
         try:
-            print(data)
-            saveFile = open('tweetDB.csv', 'a')
-            saveFile.write(data)
-            saveFile.write('\n')
-            saveFile.close()
+            json_data = json.loads(data)
+            created_at = json_data['created_at']
+            text = json_data['text']
+            lang = json_data['user']['lang']
+            user_id = str(json_data['user']['id'])
+
+            info_from_tweet = 'USER_ID:' + user_id + ' CREATED_AT:' + created_at + ' TEXT:' + text + \
+                              ' LANG:' + lang
+            save_file = open('tweetDB_using_lot_of_track_names.csv', 'a')
+            save_file.write(str(info_from_tweet))
+            save_file.write('\n')
+            save_file.close()
             return True
         except BaseException, e:
-            print 'failed ondata, ', str(e)
+            print('Failed', str(e))
+            # print (str(e)
             time.sleep(5)
         
-    def on_error(self, status):
-        print(status)
+    def on_error(self, status_code):
+        if status_code == 420:
+            # retornar Falso ira desconectar o stream de dados
+            return False
 
-
-auth = OAuthHandler(consumer_key, consumer_key)
-auth.set_access_token(access_token, access_token)
-twitterStream = Stream(auth, listener())
-twitterStream.filter(track=["depression"])
+auth = OAuthHandler(consumer_key=consumer_key, consumer_secret=consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+twitterStream = Stream(auth, Listener())
+twitterStream.filter(track=["depression", "depressive", "depressive life", "depressive mood", "depressive depression"],
+                     async=True)
